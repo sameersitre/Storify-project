e0174270dc61202f5cc6924376e43a7aa69ba57c
Sameer <sameersitre@gmail.com>
2018-09-22 16:56:56 +0530

    added responsive grid

--- client/src/_components/Home.js
+++ client/src/_components/Home.js
@@ -1,32 +1,47 @@
 import React, { Component } from 'react';
 import './Home.css';
 import Stories from '../_data/StoriesDB.json';
-import Practice from './Practice';
-//import Card from './Card'
 import StoryCard from './StoryCard';
 //import MediaCard from './MediaCard';
 import NavBar from './NavBar';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+const styles = theme => ({
+    paper: {
+        height: 350,
+        width: 300,
+    },
+});
 class Home extends Component {
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //         products: toysjson,
-    //     }
-    // }
     render() {
+        const { classes } = this.props;
         return (
             <div>
-
+                
+             <NavBar />
+                <div className='slide-container'>
+            {/* <div>
                 <NavBar />
                 <h3 align="center" className="collab"> STORY COLLABORATIONS</h3>
                 <div className='hor-slide-container'  >
-                    {Stories.map((game, i) => <StoryCard key={i} products={game} />)}
+                    {Stories.map((game, i) => <StoryCard products={game} key={i}  />)}
                 </div>
-            </div>
+            </div> */}
+           
+                <Grid container className={classes.demo} justify="center" spacing={32}>
+                    {Stories.map((value, index) =>
+                        (<Grid val={value} item={true}>
+                            <StoryCard className={classes.paper} products={value} key={index} />
+                        </Grid>
+                        ))}
+                </Grid>
+            </div></div>
         );
     }
 }
 
-
-
-export default Home;
+Home.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(Home);
--- client/src/_components/StoryCard.js
+++ client/src/_components/StoryCard.js
@@ -5,18 +5,13 @@ import Card from '@material-ui/core/Card';
 import transitions from '../../node_modules/@material-ui/core/styles/transitions';
 import Avatar from '@material-ui/core/Avatar';
 import Tooltip from '@material-ui/core/Tooltip';
-
 import './StoryCard.css';
-import Users from '../_data/Users.json'
 class StoryCard extends Component {
     constructor() {
         super();
         this.state = {
             productIndex: '',
             productImageName: '',
-            clickedProductDescription: '',
-            userimage: Users,
-            backgroundColor:'rgba(228, 228, 228, 0.78)'
         }
     }
     cardClick = (id, imgname, e) => {
@@ -25,14 +20,7 @@ class StoryCard extends Component {
         alert(id + " " + imgname + " card clicked");
     }
 
-    cardStyle = {
-        padding: '0px 10px 10px 10px',
-        margin: '20px',
-        borderRadius: '5px',
-        backgroundColor: 'white',
-        cursor:'pointer',
-    }
-
+   
     render() {
 
         if (this.state.productIndex !== '') {
@@ -40,26 +28,28 @@ class StoryCard extends Component {
 
             return <Redirect to='/ViewStory'  />
         }
+        
         return (
-            <div style={this.cardStyle}
+            <div className="card"
                 onClick={(e) =>
-                    this.cardClick(this.props.products.id, this.props.products.ImageAddress, e)} >
-                <div className="info"  >
+                    this.cardClick(this.props.products.id, this.props.products.ImageAddress, e)} 
+                >
+               
                     <h2>{this.props.products.StoryName}</h2>
-                    <div  align="left">{this.props.products.Description}</div><br />
+                    <div >{this.props.products.Description}</div><br />
+ 
 
-                </div>
-
-                <div  >
-                    {/* {console.log(this.props.products.Users)} */}
-                    {this.props.products.Users ? this.props.products.Users.map((image0, i) =>
+               
+                   
+                    { this.props.products.Users.map((image0, i) =>
                         <Tooltip title={image0.ImageAddress.slice(0, -5)} >
+
                             <Avatar style={{ display: "inline-block", float: "right", bottom:'1px'}}
                                 src={require('../photos/' + image0.ImageAddress)}
                                 alt="nothing" />
-                        </Tooltip>) : ''}
-                </div>
-                <Button size="small" color="primary"> Read</Button>
+                        </Tooltip>) }
+               
+                
             </div>
         );
     }
