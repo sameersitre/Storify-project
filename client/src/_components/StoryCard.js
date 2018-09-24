79bceedddfe26498e48d55ac99f801e238f037df
Sameer <sameersitre@gmail.com>
2018-09-24 14:31:52 +0530

    avatar positions changed

--- client/src/_components/StoryCard.js
+++ client/src/_components/StoryCard.js
@@ -12,32 +12,36 @@ class StoryCard extends Component {
         }
     }
     cardClick = (id, imgname, e) => {
-        this.setState({ productIndex: id });
-        this.setState({ productImageName: imgname })
+        this.setState({ productIndex: id, productImageName: imgname });
+
         alert(id + " " + imgname + " card clicked");
     }
 
     render() {
         if (this.state.productIndex !== '') {
-            return <Redirect to='/ViewStory'  />
+            return <Redirect to='/ViewStory' />
         }
-        
+
         return (
             <div className="card"
                 onClick={(e) =>
-                    this.cardClick(this.props.products.id, this.props.products.ImageAddress, e)} 
-                >
-                    <h2>{this.props.products.StoryName}</h2>
-                    <div >{this.props.products.Description}</div><br />
-                    
-               <div className="avatarPosition" >  
-                    { this.props.products.Users.map((image0, i) =>
+                    this.cardClick(this.props.products.id, this.props.products.ImageAddress, e)}
+            >
+
+                <h2>{this.props.products.StoryName}</h2>
+                  <div className="description">{this.props.products.Description}</div><br />
+                <div className="avatarPosition" >
+                    Authors :
+                    {this.props.products.Users.map((image0, i) =>
                         <Tooltip title={image0.ImageAddress.slice(0, -5)} >
-                            <Avatar style={{ display: "inline-block", float: "right", bottom:'1px'}}
+                            <Avatar style={{ display: "inline-block", float: "right", bottom: '1px' }}
                                 src={require('../photos/' + image0.ImageAddress)}
                                 alt="nothing" />
-                        </Tooltip>) }
-                </div>     
+                        </Tooltip>)}
+                </div>
+
+
+              
             </div>
         );
     }
